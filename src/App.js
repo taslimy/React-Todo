20e9ccf9b0782aa8bebb00d48e115e14cd88d2dc
taslimy <taslimer@gmail.com>
2019-04-25 18:26:16 -0400

    Added a if statement so you cannot add an empty string of an item

--- src/App.js
+++ src/App.js
@@ -45,6 +45,8 @@ class App extends React.Component {
 
   newToDo = event => {
     event.preventDefault();
+    if ((this.state.task == "")) {return;}
+    event.preventDefault();
     const newToDoItem = {
       task: this.state.task,
       id: Date.now(),
